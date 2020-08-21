80d8bb6426db697b8b2a0e573ed9d3a708584606
gfcodearq <69319238+gfcodearq@users.noreply.github.com>
2020-08-20 21:18:51 -0300

    Add files via upload

---
+++ MOCHILA ASH.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+class Mochila{
+private:
+	
+public: 
+	Mochila(); // constructor
+	void abrirMochila();
+	void ingresarItem();
+	void sacarItem();
+	void cerrarMochila();
+	
+};
+
+class Pokeballs{
+private: 
+	string tipo;
+public:
+	int cantidad;
+};
+
+class Obj{
+	
+};
+
+class ObjClave{
+	
+};
+
+class MT_MO{
+	
+};
+
+int main(int argc, char *argv[]) {
+	
+	return 0;
+}
+
