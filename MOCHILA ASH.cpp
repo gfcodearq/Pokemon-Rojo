c5add5d521c91d71a1c25b80bed4268bffe78426
gfcodearq <69319238+gfcodearq@users.noreply.github.com>
2020-08-21 14:35:13 -0300

    Add files via upload

--- MOCHILA ASH.cpp
+++ MOCHILA ASH.cpp
@@ -1,37 +1,70 @@
 #include <iostream>
 using namespace std;
+
 class Mochila{
 private:
 	
-public: 
+public: //metodos
 	Mochila(); // constructor
-	void abrirMochila();
+	void abrirMochila(); 
 	void ingresarItem();
 	void sacarItem();
 	void cerrarMochila();	
 };
 
-class Objetos{
-	private: 
-	string tipo;
+void Mochila::abrirMochila(){
+	cout<<"Abriendo la mochila"<<endl;
+}
+
+void Mochila::ingresarItem(){
+	cout<<"Que item desea ingresar"<<endl;
+}
+
+void Mochila::cerrarMochila(){
+	cout<<"Se ha cerrado la mochila"<<endl;
+}
+
+class Objeto{
+	private: //atributos
+	string tipo; 
 	string nombre;
 	int precio;
-public:
 	int cantidad;
-	bool cantidad;
+public: //metodos
+	Objeto(string,string,int,int); //constructor		
+	void setTipo(); //setters
+	void setNombre(); //setters
+	void setPrecio(); //setters
+	void getTipo(); //getters
+	void getNombre(); //getters
+	void getPrecio(); //getterts
+	bool verificar_cantidad();
 };	
 
+Objeto::Objeto(string _tipo,string _nombre, int _precio,int _cantidad){ //constructor
+	tipo = _tipo;
+	nombre = _nombre;
+	precio = _precio;
+	cantidad = _cantidad;
+}
 
-class Pokeballs : public Objetos{
+void Objeto::setTipo(){
+	cout<<"Que tipo de objeto es: "<<endl;
+	cin>>tipo;
+}
+
+
+
+class Pokeball : public Objeto{
 };
 
-class Obj : public Objetos{
+class Obj : public Objeto{
 };
 
-class ObjClave : public Objetos{
+class ObjClave : public Objeto{
 };
 
-class MT_MO : public Objetos{
+class MT_MO : public Objeto{
 	
 };
 
