257c1252b3e6ae51a944b329e5ef2bdfd5f33d34
gfcodearq <69319238+gfcodearq@users.noreply.github.com>
2020-08-23 09:34:32 -0300

    Add files via upload
    complete clase objeto

--- MOCHILA ASH.cpp
+++ MOCHILA ASH.cpp
@@ -25,37 +25,57 @@ void Mochila::cerrarMochila(){
 }
 
 class Objeto{
-	private: //atributos
-	string tipo; 
+	protected: //atributos
+	int numero;
 	string nombre;
 	int precio;
 	int cantidad;
 public: //metodos
-	Objeto(string,string,int,int); //constructor		
-	void setTipo(); //setters
-	void setNombre(); //setters
-	void setPrecio(); //setters
-	void getTipo(); //getters
+	Objeto(int,string,int,int); //constructor		
+	void setDatos(); //setters
 	void getNombre(); //getters
 	void getPrecio(); //getterts
+	void getDatos(); //getters
 	bool verificar_cantidad();
 };	
 
-Objeto::Objeto(string _tipo,string _nombre, int _precio,int _cantidad){ //constructor
-	tipo = _tipo;
+Objeto::Objeto(int _numero,string _nombre, int _precio,int _cantidad){ //constructor
+	numero = _numero;
 	nombre = _nombre;
 	precio = _precio;
 	cantidad = _cantidad;
 }
 
-void Objeto::setTipo(){
-	cout<<"Que tipo de objeto es: "<<endl;
-	cin>>tipo;
+void Objeto::setDatos(){
+	cout<<"Numero del objeto: "<<endl;
+	cin>>numero;
+	cout<<"Cual es el nombre del objeto: "<<endl;
+	cin>>nombre;
+	cout<<"Cual es el precio: "<<endl;
+	cin>>precio;
+	cout<<"Cuantos ingresa: "<<endl;
+	cin>>cantidad;
 }
 
 
+void Objeto::getDatos(){
+	cout<<"N°: "<<numero<<endl;
+	cout<<"Nombre: "<<nombre<<endl;
+	cout<<"Precio: "<<precio<<endl;
+	cout<<"Cantidad: "<<cantidad<<endl;
+}
+
+void Objeto::getPrecio(){
+	cout<<"Precio: "<<precio<<endl;
+}
+
+void Objeto::getNombre(){
+	cout<<"Nombre: "<<nombre<<endl;
+}
 
 class Pokeball : public Objeto{
+	public:
+		Pokeball(int,string,int,int) : Objeto (numero,nombre,precio,cantidad);
 };
 
 class Obj : public Objeto{
@@ -69,6 +89,14 @@ class MT_MO : public Objeto{
 };
 
 int main(int argc, char *argv[]) {
+	
+	Objeto Obj1(006,"bicicleta",0,1);
+	Obj1.setDatos();
+	Obj1.getDatos();
+	
+	Pokeball pkbl();
+	pkbl.setDatos();
+	pkbl.getDatos();
 	
 	return 0;
 }
