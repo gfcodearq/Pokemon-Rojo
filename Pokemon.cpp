9dbb63a293c47ca331f888f96cb15a3cff72f68c
gfcodearq <fernandez_guillermo@outlook.com>
2020-08-29 18:07:01 -0300

    modifique archivo pokemon

--- Pokemon.cpp
+++ Pokemon.cpp
@@ -3,29 +3,31 @@
 using namespace std;
 
 class Pokemon{
-	private: //atributos
+protected: //atributos
 	string nombre;
+	string tipo;
 	float peso;
 	float altura;
 	int nivel;
-	int hp; //vida
+	int ps; //vida
 	string sonido;
 	
-	public:
-		Pokemon(string,float,float,int,int,string);
-		void setDatos();
-		void getDatos();
-		void atacar();
-		void recibirDanio();
-		virtual string playsonido();		
+public:
+	Pokemon(string,string,float,float,int,int,string);
+	void setDatos();
+	void getDatos();
+	void atacar();
+	void recibirDanio();
+	virtual void playsonido();		
 };
 
-Pokemon::Pokemon(string _nombre,float _peso,float _altura,int _nivel, int _hp, string _sonido){
+Pokemon::Pokemon(string _nombre,string _tipo,float _peso,float _altura,int _nivel, int _ps, string _sonido){
 	nombre = _nombre;
+	tipo = _tipo;
 	peso = _peso;
 	altura = _altura;
 	nivel = _nivel;
-	hp = _hp;
+	ps = _ps;
 	sonido = _sonido;
 }
 
@@ -38,13 +40,35 @@ void Pokemon::setDatos(){
 	cin>>altura;
 	cout<<"Digite el nivel: "<<endl;
 	cin>>nivel;
-	cout<<"Hp actual: "<<endl;
-	cin>>hp;
+	cout<<"ps actual: "<<endl;
+	cin>>ps;
 	cout<<"Cual es el sonido"<<endl;
 }
 
-int main(int argc, char *argv[]) {	
-	Pokemon Pikachu();
+void Pokemon::getDatos(){
+	cout<<"Nombre: "<<nombre<<endl;
+	cout<<"Peso: "<<peso<<"Kg"<<endl;
+	cout<<"Altura: "<<altura<<"cm"<<endl;
+	cout<<"Nivel: "<<nivel<<endl;
+	cout<<"Ps: "<<ps<<endl;
+	cout<<"Sonido: "<<sonido<<endl;
+}
+
+void Pokemon::atacar(){
+}
+
+void Pokemon::recibirDanio(){
+}
+
+void Pokemon::playsonido(){
 	
+}
+
+int main(int argc, char *argv[]) {	
+	Pokemon Pikachu("Pikachu","Electrico",6,0.4,5,31,"Pika Pika");
+	//Pokemon Bulbasor("Bulbasaur","Semilla",6.9,0.7,31,"Bulbasaur");
+	Pikachu.getDatos();
 	return 0;
 }
+
+
